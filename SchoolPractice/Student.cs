70ae433b32fe51dd7a1865222cdfa5e60d9afde0
lcc <sub01@iaonline.net>
2020-10-08 17:00:58 -0500

    Completed Class 4 Exercises Ch. 5.5.

--- SchoolPractice/Student.cs
+++ SchoolPractice/Student.cs
@@ -27,37 +27,16 @@ namespace SchoolPractice
             nextStudentId++;
         }
 
-        // TODO: Complete the AddGrade method.
         public void AddGrade(int courseCredits, double grade)
         {
             // Update the appropriate properties: NumberOfCredits, Gpa
-            /*
-             * This method accepts two parameters—a number of course credits and a numerical grade (0.0-4.0). With this data, you need to update the student’s GPA.
-
-                5.5.2.1. GPA Information
-                    GPA is computed via the formula:
-
-                    gpa = (total quality score) / (total number of credits)
-                    The quality score for a class is found by multiplying the letter grade score (0.0-4.0) by the number of credits.
-                    The total quality score is the sum of the quality scores for all classes.
-                    For example, if a student received an “A” (worth 4 points) in a 3-credit course and a “B” (worth 3 points) in a 4-credit course,
-                    their total quality score would be: 4.0 * 3 + 3.0 * 4 = 24. Their GPA would then be 24 / 7 = 3.43.
-
-                5.5.2.2. Determine the New GPA
-                    To update the student’s GPA:
-
-                    Calculate their current total quality score by using the formula gpa * numberOfCredits.
-                    Use the new course grade and course credits to update their total quality score.
-                    Update the student’s total numberOfCredits.
-                    Compute their new GPA.
-             */
             double oldTotalQualityScore = Gpa * NumberOfCredits;
             double newTotalQualityScore = oldTotalQualityScore + courseCredits * grade;
 
+            NumberOfCredits += courseCredits;
             Gpa = newTotalQualityScore / NumberOfCredits;
         }
 
-        //TODO: Complete the GetGradeLevel method here:
         public string GetGradeLevel(int credits)
         {
             // Determine the grade level of the student based on NumberOfCredits
@@ -80,11 +59,26 @@ namespace SchoolPractice
             return "Senior";
         }
 
-        // TODO: Add your custom 'ToString' method here. Make sure it returns a well-formatted string rather
-        //  than just the class fields.
-
         // TODO: Add your custom 'Equals' method here. Consider which fields should match in order to call two
         //  Student objects equal.
+        public override bool Equals(object obj)
+        {
+            return obj is Student student &&
+                   Name == student.Name &&
+                   StudentId == student.StudentId;
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(Name, StudentId);
+        }
+
+        // TODO: Add your custom 'ToString' method here. Make sure it returns a well-formatted string rather
+        //  than just the class fields.
+        public override string ToString()
+        {
+            return "Name: " + Name + "\nStudent ID: " + StudentId + "\GPA: " + Gpa + "\nCredits: " + NumberOfCredits;
+        }
 
     }
 }
